f21b53b5a564ab286edbf9e0abe6dc00e96a33e3
Devesh Kumar <105088428+azrael18102001@users.noreply.github.com>
2023-05-15 10:04:19 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
-sudo apt-get update && apt-get install -y libsndfile1 ffmpeg
+sudo apt-get update && sudo apt-get install -y libsndfile1 ffmpeg
 pip install Cython
 conda install libgcc gmp
 pip install nemo_toolkit['all']
-
 conda install -c conda-forge
